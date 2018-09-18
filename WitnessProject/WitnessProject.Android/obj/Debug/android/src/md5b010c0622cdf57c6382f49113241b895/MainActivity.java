a8597997e0704cb989d0775f13cff89ad99c48d5
LaVannak <Vannak.la@gmail.com>
2018-09-18 23:08:27 +0800

    Witness Application

---
+++ WitnessProject/WitnessProject.Android/obj/Debug/android/src/md5b010c0622cdf57c6382f49113241b895/MainActivity.java
@@ -0,0 +1,47 @@
+package md5b010c0622cdf57c6382f49113241b895;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("WitnessProject.Droid.MainActivity, WitnessProject.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("WitnessProject.Droid.MainActivity, WitnessProject.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ WitnessProject/WitnessProject/Pages/HomePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WitnessProject.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void TapHelp_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HelpPage());
+        }
+
+        private void TapViewIncident_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new IncidentView());
+        }
+
+        private void TapNewIncident_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewIncidentPage());
+        }
+
+        private void TapMyIncident_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new IncidentView());
+        }
+    }
+}
